fd6f9e8f4070408289c5ac087d25d80acc312c19
gnal <qwertypianox@gmail.com>
2012-07-25 14:00:17 -0400

    fix

--- src/Msi/Bundle/MainBundle/Menu/Builder.php
+++ src/Msi/Bundle/MainBundle/Menu/Builder.php
@@ -15,7 +15,7 @@ class Builder extends ContainerAware
         $menu = $factory->createFromNode($root);
 
         foreach ($menu as $m) {
-            if ($pathInfo === $m->getUri() || preg_match('@^'.$pathInfo.'@', $m->getUri())) {
+            if ($pathInfo === $m->getUri() || ($m->getUri() !== '/' && preg_match('@^'.$m->getUri().'@', $pathInfo))) {
                 $m->setCurrent(true);
             }
         }
